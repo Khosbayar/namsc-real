36a187cf5633d2c6a18ce272a98783c2eb915f40
Soyol <soyol0616@gmail.com>
2015-10-24 15:14:44 +0800

    v1.1
    Header bolson

---
+++ about/index.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<section class="header_section">
+			<header>
+				<div class="container">
+					<div class="logo">
+						<a href="">
+							<img src="../public/imgs/logo.png" />
+						</a>
+					</div>
+					<div class="menu">
+						<nav>
+							<ul>
+								<li><a href="/">Нүүр</a></li>
+								<li><a class="active" href="/about">Тухай</a></li>
+								<li><a href="/clubs">Клуб</a></li>
+								<li><a href="/calendar">Хуваарь</a></li>
+								<li><a href="/album">Зураг</a></li>
+								<li><a href="/contact">Холбоо барих</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+		</section>
+		<div class="content">
+			<div class="container">
+				About
+			</div>
+		</div>
+	</body>
+</html>
